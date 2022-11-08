ed7bdc0b44b86b8a19f9665c9d7d8fea45612245
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-11-08 12:37:03 +0530

    Initial Commit

---
+++ src/main/java/com/javagenericsproblem/App.java
@@ -0,0 +1,9 @@
+package com.javagenericsproblem;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Java Generics Project" );
+    }
+}
